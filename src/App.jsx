f8105efddb88cada03f9a52318d6ac7357332ae7
ikhwanhsn <ikhwanulhusna111@gmail.com>
2023-07-27 08:24:17 +0700

    update view

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Navbar from "./components/Navbar"
+
 function App() {
 
   return (
-    <div className="text-center ">
-      <h1 className="mt-24 mb-8 text-3xl font-bold">
-        Template Vite (React + Tailwind)
-      </h1>
-      <a href="https://github.com/ikhwanhsn/template_vite_reactTailwind" target='_blank' className='px-3 py-2 text-sm text-white transition-all bg-blue-400 rounded-full shadow-md hover:bg-blue-500 hover:shadow-lg'>Check Version</a>
+    <div className="w-full h-screen bg-color-four">
+      <Navbar/>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-one': '#4A55A2',
+        'color-two': '#7895CB',
+        'color-tree': '#A0BFE0',
+        'color-four': '#C5DFF8',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
